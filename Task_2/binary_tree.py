ca4d09280e5de50cd5609627648597c36930d6e7
John Chung <john@gmail.com>
2026-03-08 23:09:33 +0800

    Task 2 Work

--- Task_2/binary_tree.py
+++ Task_2/binary_tree.py
@@ -1,2 +1,22 @@
 """Binary Tree"""
 
+#The Binary tree is a non-linear and hierarchical data structure. It's each node has at most two children.
+#Below is a node structure
+
+# Node structure
+class Node:
+    def __init__(self, d):
+        self.data = d
+        self.left = None
+        self.right = None
+
+# Initialize and allocate memory for tree nodes
+firstNode = Node(2)
+secondNode = Node(3)
+thirdNode = Node(4)
+fourthNode = Node(5)
+
+# Connect binary tree nodes
+firstNode.left = secondNode
+firstNode.right = thirdNode
+secondNode.left = fourthNode
